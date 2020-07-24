d457d6b5ccb7338d3d2e8785a476ae87c742475b
Umberto <umbertopellegrino96@gmail.com>
2020-07-24 10:31:56 +0200

    Change month

--- js/jscript.js
+++ js/jscript.js
@@ -1,18 +1,29 @@
-
-function getDays() {
+function changeMonth() {
+  var selectMonth = $('.mesi');
+  var value;
+  selectMonth.change(function () {
+    value = selectMonth.val()
+    getDays(value);
+  })
+}
+function getDays(value) {
   var target = $('#target-month');
   target.text("");
-  var currentMonth = moment('2018-01');
+  var currentMonth = moment('2018-'+value,'YYYY-M');
   var howManyDays = currentMonth.daysInMonth();
 
   var template = $('#month-template').html();
   var compiled = Handlebars.compile(template);
 
+  var month2018 = $('#month-2018');
+
+  month2018.text(currentMonth.format('MMMM')+'-2018');
+
   for (var i = 1; i <= howManyDays; i++) {
     var dateComplete = moment({year:currentMonth.year(), month:currentMonth.month(),day:i});
     var daysHTML = compiled({
       'dateComplete': dateComplete.format('YYYY-MM-DD'),
-      'day': i
+      'day': i +' - '+ dateComplete.format('dddd')
     });
     target.append(daysHTML)
   }
@@ -51,6 +62,7 @@ function checkHoliday(arrayHoliday) {
 }
 
 function init() {
+  changeMonth();
   getDays();
 }
 
